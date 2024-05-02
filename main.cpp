bdc4186449cc6e3a64fd2130b115e047c1b0157a
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-02 23:51:41 +0545

    feat: add simple driver file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <cstring>
+#include <iostream>
+#include "lexer.hpp"
+#include "parser.hpp"
+
+
+int main(int argc, char* argv[]){
+  const bool debug = argc > 1 && std::strcmp(argv[1], "--debug") == 0;
+
+  STAB::Lexer lexer(std::cin, debug);
+  STAB::BisonParser parser(lexer, debug);
+
+  return parser();
+}
