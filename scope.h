a1a5e2f76cb9ce0b8ee89cf6a4111a44bd4043f5
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-05 22:43:08 +0545

    feat: add two different symtable, one for vars and other for fns

--- scope.h
+++ scope.h
@@ -2,16 +2,35 @@
 #define SCOPE_H
 
 #include <iostream>
+#include <algorithm>
+#include <utility>
 #include <vector>
+#include <map>
 #include "AST.h"
+
+struct FnTable{
+  std::string fnName;
+  std::string retType;
+  // key: variable, value: type
+  std::map<std::string, std::string> args;
+
+  std::string getFnName(){ return fnName;}
+  std::string getretType(){return retType;}
+  const std::map<std::string, std::string>& getArgs() const{
+   return args;
+  }
+};
+
 class Scope {
   public: 
    // no need for data hiding
    Scope(Scope* e = nullptr){
-     symbolTable = {}; 
+     IdTable = {}; 
+     fnTable = {};
      prev = e;
    }
-   std::vector<ExprAST*> symbolTable;
+   std::map<std::string, std::string> IdTable;
+   std::vector<FnTable> fnTable;
    // points to its parent scope, 
    // for nested scope
    Scope* prev;
@@ -26,6 +45,35 @@ class Scope {
     return out;
    }
    */
+   // install variable name to the idTable
+   void installID(std::string key, std::string value){
+      IdTable.emplace(std::make_pair(key, value));
+   }
+
+   // check if a variable name already exists within the current scope
+   bool idExists(std::string id) const{
+     return !(IdTable.find(id) == IdTable.end());
+   }
+
+   // install function to the funciton Table
+   void installFn(std::string name, std::string type, std::map<std::string, std::string> args){
+    FnTable newEntry = {
+	 name,
+	 type,
+	 args
+    };
+    fnTable.emplace_back(newEntry);
+   }
+
+   // check if a function with similar signature already exists or not
+   bool FnExists(std::string id) const{
+     for (const auto& elt: fnTable){
+	if(elt.fnName == id) return true;     
+     } 
+     return false;
+   }
+
+
  };
 
 #endif
