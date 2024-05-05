664496aa66505dd284b9fa083b89d40a95faabad
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-05 19:52:27 +0545

    feat: symbolTable is now vec of ExprAST

--- scope.h
+++ scope.h
@@ -2,34 +2,30 @@
 #define SCOPE_H
 
 #include <iostream>
-#include <string>
 #include <vector>
+#include "AST.h"
 class Scope {
   public: 
    // no need for data hiding
    Scope(Scope* e = nullptr){
      symbolTable = {}; 
-     numberTable = {};
      prev = e;
    }
-   std::vector<std::string> symbolTable;
-   std::vector<std::string> numberTable;
+   std::vector<ExprAST*> symbolTable;
    // points to its parent scope, 
    // for nested scope
    Scope* prev;
-
+   /*
    friend std::ostream& operator << (std::ostream& out, Scope* e){
     while(e != nullptr){
       for(auto elt: e->symbolTable){
-        out << elt << " "; 
-      }
-      for(auto elt: e->numberTable){
         out << elt << " "; 
       }
       e = e->prev;
     }
     return out;
    }
+   */
  };
 
 #endif
