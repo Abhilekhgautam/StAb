fb1bcc6fe9b4bc9e09cd0d76401a44a704c82d9c
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-05 19:51:16 +0545

    refactor: BisonParser to Parser

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]){
   const bool debug = argc > 1 && std::strcmp(argv[1], "--debug") == 0;
 
   STAB::Lexer lexer(std::cin, debug);
-  STAB::BisonParser parser(lexer, debug);
+  STAB::Parser parser(lexer, debug);
 
   return parser();
 }
