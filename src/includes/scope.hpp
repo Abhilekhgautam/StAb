aa05513a577ecf9b9735095c1bf85eb3c173ec3a
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-24 00:00:21 +0545

    happy birthday abhilekh

---
+++ src/includes/scope.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#ifndef STAB_SCOPE_HPP
+#define STAB_SCOPE_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <map>
+
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instructions.h"
+#include <llvm/IR/Function.h>
+
+namespace STAB{
+    class Scope {
+       std::map<std::string, llvm::AllocaInst*> SymbolTable;
+       std::string name;
+       llvm::Function* F;
+      public:
+        Scope* prev;
+       Scope(Scope* e = nullptr, std::string name = ""):name(name), prev(e){}
+
+       void setFnBlock(llvm::Function* Fn);
+
+       void createFnBlock();
+
+       llvm::Function* getFnBlock();
+       std::string getName() const;
+
+       // install variable name to the idTable
+       void installID(std::string key, llvm::AllocaInst*  value);
+
+       // check if a variable name already exists within the current scope
+       llvm::AllocaInst* getID(std::string id) const;
+
+ };
+}
+#endif
