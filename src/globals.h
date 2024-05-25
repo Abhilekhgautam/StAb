155c06b7e6b84a38181dcea590327d2492a333ec
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-25 22:57:32 +0545

    feat: remove internal main fn

--- src/globals.h
+++ src/globals.h
@@ -17,15 +17,23 @@
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/Function.h>
 
-#include "./includes/scope.h"
+#include "./includes/scope.hpp"
+#include "./simple-ast/ast.h"
+
+namespace STAB{
+    class Scope;
+    class FunctionAST;
+}
 
 extern std::unique_ptr<llvm::LLVMContext> TheContext;
 extern std::unique_ptr<llvm::IRBuilder<>> Builder;
 extern std::unique_ptr<llvm::Module> TheModule;
 extern std::map<std::string, llvm::Type*> NamedLLVMType;
 extern std::map<std::string, llvm::AllocaInst *> NamedValues;
-extern llvm::Function* mainFunction;
-
+extern STAB::Scope* globalScope;
+extern STAB::Scope* currentScope;
+extern STAB::FunctionAST* __start__fn;
+extern std::vector<llvm::Function*> fnBlocks;
 /*
 void addNewScope(Scope* s){
    scopes.emplace_back(s);	
@@ -39,17 +47,6 @@ inline void initializeModule(){
      // Create a new builder for the module.
      Builder = std::make_unique<llvm::IRBuilder<>>(*TheContext);
      std::vector<llvm::Type* > argTypes;
-     llvm::FunctionType *ftype = llvm::FunctionType::get(llvm::Type::getVoidTy(*TheContext), argTypes, false);
-     mainFunction = llvm::Function::Create(ftype,llvm::GlobalValue::InternalLinkage, "main", *TheModule);
 }
 
-/// CreateEntryBlockAlloca - Create an alloca instruction in the entry block of
-/// the function.  This is used for mutable variables etc.
-static llvm::AllocaInst *CreateEntryBlockAlloca(llvm::Function *TheFunction,
-                                          const std::string &VarName) {
-	llvm::IRBuilder<> TmpB(&TheFunction->getEntryBlock(),
-                 TheFunction->getEntryBlock().begin());
-  return TmpB.CreateAlloca(llvm::Type::getDoubleTy(*TheContext), nullptr,
-                           VarName);
-}
 #endif 
