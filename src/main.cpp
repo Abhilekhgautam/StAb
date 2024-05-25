27a542b3a06bac5b4c1e12e948461af6120f9807
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-25 23:03:32 +0545

    feat: check for the existence of a main function. Create a vector for all the function defined and generate IR accordingly

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <cstring>
 #include <iostream>
 #include "./lexer/lexer.hpp"
-#include "includes/scope.h"
+#include "includes/scope.hpp"
 #include <map>
 #include <string>
 #include <memory>
@@ -19,7 +19,7 @@
 #ifndef STAB_GLOBAL_H
 #include "globals.h"
 #endif
-#ifndef STAB_SCOPE_H
+#ifndef STAB_SCOPE_HPP
 #include "./includes/scope.h"
 #endif
 
@@ -27,9 +27,11 @@ std::unique_ptr<llvm::LLVMContext> TheContext;
 std::unique_ptr<llvm::IRBuilder<>> Builder;
 std::map<std::string, llvm::Type*> NamedLLVMType;
 std::unique_ptr<llvm::Module> TheModule;
-llvm::Function* mainFunction;
 std::map<std::string, llvm::AllocaInst *> NamedValues;
 std::vector<STAB::Scope*> scopes;
+STAB::FunctionAST* __start__fn;
+
+std::vector<llvm::Function*> fnBlocks;
 
 STAB::Scope* globalScope = nullptr;
 STAB::Scope* currentScope = globalScope;
@@ -56,9 +58,27 @@ int main(int argc, char* argv[]){
   STAB::Lexer lexer(source_file, debug);
   STAB::Parser parser(lexer, debug);
 
-  // print the generated code..
-  // TheModule->print(llvm::errs(), nullptr);
-
   parser();
+
+  if(__start__fn){
+       __start__fn->codegen(nullptr);
+
+      // Get the main function 
+      auto main = TheModule->getFunction("main");
+
+      if (!main){
+          std::cerr << "\nError: No main function defined.\nSTAB requires at least a\"main\" function for execution\n";     
+	  return -1;
+      }
+
+      // get all the function definitions
+      for(auto const elt: fnBlocks){
+	  // print the generated code
+	  elt->print(llvm::errs());    
+      }
+      
+  } else {
+      std::cerr << "__start__fn fn not defined yet!!\n";
+  }
 
 }
