3e090fceb29d6ab0f7adc1a56164e738e2d7bbe5
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-26 23:41:15 +0545

    feat: define string as a type

---
+++ src/simple-ast/stringExprAST.cpp
@@ -0,0 +1,11 @@
+//
+// Created by abhilekh on 5/26/24.
+//
+
+#include "./ast.h"
+
+namespace STAB{
+    llvm::Value* STAB::StringExprAST::codegen(Scope* s){
+	return Builder->CreateGlobalString(val);
+    }
+}
