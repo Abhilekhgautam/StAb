806f8ab8663b096916b93f20f81124ea73767d1a
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-26 23:40:58 +0545

    feat: define fn call as both expr and stmt

---
+++ src/simple-ast/callStatementAST.cpp
@@ -0,0 +1,34 @@
+//
+// Created by abhilekh on 5/26/24.
+//
+
+#include "./ast.h"
+
+namespace STAB{
+    llvm::Value* STAB::CallStatementAST::codegen(Scope* s){
+        llvm::Function* calleeFn = TheModule->getFunction(Callee);
+        if (Callee == "println"){
+	     auto printlnFunc = TheModule->getOrInsertFunction("println", llvm::FunctionType::get(llvm::IntegerType::getInt32Ty(*TheContext), llvm::PointerType::get(llvm::Type::getInt8Ty(*TheContext), 0), true));
+	     std::vector<llvm::Value*> ArgsV;
+             for (unsigned i = 0, e = Args.size(); i != e; ++i)
+                 ArgsV.push_back(Args[i]->codegen(s));
+
+             return Builder->CreateCall(printlnFunc, ArgsV, "printlnCall");
+	}
+
+        if (!calleeFn){
+            std::cerr << "\nNo function" << Callee << "exist\n";
+            return nullptr;
+        }
+        // check the number of arguments passed
+        if(calleeFn->arg_size() != Args.size()){
+            std::cerr << "\n Function " << Callee << " expects " << calleeFn->arg_size() << " but " << Args.size() << " were passed\n";
+            return nullptr;
+        }
+        std::vector<llvm::Value*> args;
+        for(const auto expr: Args){
+            args.emplace_back(expr->codegen(s));
+        }
+        return Builder->CreateCall(calleeFn, args, "calltmp");
+    }
+}
