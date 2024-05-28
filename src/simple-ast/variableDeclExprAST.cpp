4225c4171e76460f55bac87842f84cd8fc522077
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-28 23:32:30 +0545

    feat: add check for string type

--- src/simple-ast/variableDeclExprAST.cpp
+++ src/simple-ast/variableDeclExprAST.cpp
@@ -3,13 +3,18 @@
 //
 
 #include "./ast.h"
+#include <llvm-18/llvm/IR/DerivedTypes.h>
 
 namespace STAB{
     llvm::Value* STAB::VariableDeclExprAST::codegen(Scope* s){
-        llvm::BasicBlock* bblock = llvm::BasicBlock::Create(*TheContext, s->getName(), s->getFnBlock(), 0);
 
-        auto type = llvm::Type::getInt32Ty(*TheContext);
-        Builder->SetInsertPoint(bblock);
+        llvm::IntegerType* type = nullptr;
+
+	if(getType() == "string")
+          type = llvm::Type::getInt8Ty(*TheContext);	  	
+	else 
+          type = llvm::Type::getInt32Ty(*TheContext);
+        
         llvm::AllocaInst* var = Builder->CreateAlloca(type,nullptr, Name);
         s->installID(Name, var);
         //NamedValues[Name] = var;
