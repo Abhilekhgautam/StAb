8ddf61ada5e469726d0a6c1c8fd6d9014ca4efc2
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-05-28 22:32:16 +0545

    feat: map call arguments with function parameters

--- src/simple-ast/functionAST.cpp
+++ src/simple-ast/functionAST.cpp
@@ -1,83 +1,88 @@
 
 #include "./ast.h"
+#include <llvm-18/llvm/IR/Instructions.h>
+#include <llvm-18/llvm/IR/Value.h>
 
 namespace STAB{
-    llvm::Function* STAB::FunctionAST::codegen(class Scope* s){
-        std::vector<llvm::Type*> params(Proto->getArgSize(), llvm::Type::getInt32Ty(*TheContext));
-        llvm::FunctionType* FT;
-        if (Proto->getReturnType() == "void") {
-            FT = llvm::FunctionType::get(llvm::Type::getVoidTy(*TheContext), params, false);
-        }else {
-            FT =  llvm::FunctionType::get(llvm::Type::getInt32Ty(*TheContext), params, false);
+llvm::Function* STAB::FunctionAST::codegen(class Scope* s) {
+    std::vector<llvm::Type*> params(Proto->getArgSize(), llvm::Type::getInt32Ty(*TheContext));
+    llvm::FunctionType* FT;
+
+    // Determine the function return type
+    if (Proto->getReturnType() == "void") {
+        FT = llvm::FunctionType::get(llvm::Type::getVoidTy(*TheContext), params, false);
+    } else {
+        FT = llvm::FunctionType::get(llvm::Type::getInt32Ty(*TheContext), params, false);
+    }
+    
+    // Check if such function already exists 
+    llvm::Function* F = TheModule->getFunction(Proto->getName());
+
+    if (F) {
+        std::cerr << "\nError: Cannot have 2 functions with the same name\n.Function " << Proto->getName() << " already defined\n";
+        return nullptr;
+    }
+
+    if (Proto->getName() == "__start__") {
+        F = TheModule->getFunction("__start__");
+        if (!F) {
+            F = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, "__start__", TheModule.get());
+            llvm::BasicBlock* fnBlock = llvm::BasicBlock::Create(*TheContext, "entry", F);
+            Builder->SetInsertPoint(fnBlock);
+        } else {
+            llvm::BasicBlock &lastBlock = F->back();
+            Builder->SetInsertPoint(&lastBlock);
         }
-        
-	// Check if such function already exists 
-	llvm::Function* F = TheModule->getFunction(Proto->getName());
+    } else {
+        F = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, Proto->getName(), TheModule.get());
+        llvm::BasicBlock* fnBlock = llvm::BasicBlock::Create(*TheContext, Proto->getName(), F);
+        Builder->SetInsertPoint(fnBlock);      
+    }
 
-	if (F) {
-          std::cerr << "\nError: Cannot have 2 functions with the same name\n.Function " << Proto->getName() << " already defined\n";
-	  return nullptr;
-	}
+    fnBlocks.emplace_back(F);
+    Scope->setFnBlock(F);
 
-	if(Proto->getName() == "__start__"){
-            // Check if __start__fn already exists
-            F = TheModule->getFunction("__start__");
-            if (!F) {
-                F = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, "__start__", TheModule.get());
-                llvm::BasicBlock* fnBlock = llvm::BasicBlock::Create(*TheContext, "entry", F);
-                 Builder->SetInsertPoint(fnBlock);
-             } else {
-               // Set the insert point to the end of the existing __start__fn function
-               llvm::BasicBlock &lastBlock = F->back();
-               Builder->SetInsertPoint(&lastBlock);
-             }	   	
-	} else {
-          F = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, Proto->getName(), TheModule.get());
-          llvm::BasicBlock* fnBlock = llvm::BasicBlock::Create(*TheContext, Proto->getName(), F);
-          Builder->SetInsertPoint(fnBlock);		
-	}
+    std::vector<std::string> names;
+    // Codegen for function parameters5
+    for(const auto params: declVars) {
+	auto name = params->getName();
+	std::cerr << "\nOk got a " << name << " param baby\n";
+	params->codegen(Scope);
+	names.emplace_back(name);
+    }
 
-        fnBlocks.emplace_back(F);
+    // Store each function argument
+    unsigned idx = 0;
+    for (auto &Arg : F->args()) {
+        Builder->CreateStore(&Arg, Scope->getID(names[idx]));
+	++idx;
+    }
 
-        Scope->setFnBlock(F);
-       // llvm::BasicBlock* fnBlock = llvm::BasicBlock::Create(*TheContext, Proto->getName(), F);
-       // Builder->SetInsertPoint(fnBlock);
+    // Codegen for function body
+    for(const auto body: Body) {
+        body->codegen(Scope);
+    }
 
-        for(const auto params: declVars){
-             params->codegen(Scope);
-        }
-        for(const auto body: Body){
-           body->codegen(Scope);
-        }
-        // Check for proper termination
-        bool hasReturn = false;
-        llvm::BasicBlock* lastBlock = nullptr;
-        for (auto &BB : *F) {
-            if (BB.getTerminator()) {
-                if (llvm::isa<llvm::ReturnInst>(BB.getTerminator()) && Proto->getReturnType() == "void") {
-                    std::cerr << "\nError: Void Function cannot have a return statement\n";
-                    return nullptr;
-                } else {
-                    hasReturn = true;
-                }
-                lastBlock = &BB;
-            }
-        }
-        if(lastBlock)
-            Builder->SetInsertPoint(lastBlock);
-
+    // Ensure a return statement if none exists
+    llvm::BasicBlock* currentBlock = Builder->GetInsertBlock();
+    if (!currentBlock->getTerminator()) {
         if (Proto->getReturnType() == "void") {
             Builder->CreateRetVoid();
-        }
-        // If no return statement was found and the function is non-void, this is an error.
-        if (!hasReturn && Proto->getReturnType() != "void") {
+        } else {
             std::cerr << "\nError: Non-void function does not have a return statement\n";
             return nullptr;
         }
-
-        llvm::verifyFunction(*F);
-        // just to check the generated IR
-        // todo: Change to actual thing that will be returned
-        return F;
     }
+    // Special case for __start__
+    if (Proto->getName() == "__start__") {
+        llvm::BasicBlock* lastBlock = Builder->GetInsertBlock();
+        if (!lastBlock->getTerminator()) {
+            Builder->CreateRetVoid();
+        }
+    }
+
+    llvm::verifyFunction(*F);
+    return F;
+ }
+
 }
