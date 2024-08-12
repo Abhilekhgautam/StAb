dc07f4b8c24a98ce271b5e183fe2a57c7e9729d6
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-08-12 22:31:02 +0545

    feat: implement a method that checks for the existence of a variable in the current scope only

--- compiler/src/includes/scope.cpp
+++ compiler/src/includes/scope.cpp
@@ -6,43 +6,49 @@
 #include "../globals.h"
 #include <optional>
 
-namespace STAB{
-    void STAB::Scope::setFnBlock(llvm::Function* Fn){
-        F = Fn;
-    }
+namespace STAB {
+void STAB::Scope::setFnBlock(llvm::Function *Fn) { F = Fn; }
 
-    void STAB::Scope::createFnBlock(){
-        F = Builder->GetInsertBlock()->getParent();
-    }
-
-    llvm::Function* STAB::Scope::getFnBlock(){return F;}
-    std::string STAB::Scope::getName() const {return name;}
-
-    // install variable name to the idTable
-    void STAB::Scope::installID(std::string key, valType value) {
-        SymbolTable.insert({key, value});
-    }
-
-    // check if a variable name already exists within the current scope
-    std::optional<valType> STAB::Scope::getID(std::string id) const {
-	Scope* prevScope = prev;
-        auto found = SymbolTable.find(id);
-        if (found != SymbolTable.end()){
-            return found->second;
-        }
-	if (prevScope){
-	    auto symTable = prevScope->SymbolTable;
-	    while(prevScope){
-	        auto found = symTable.find(id);
-                if (found != symTable.end()){
-                   return found->second;
-                 }
-	        prevScope = prevScope->prev;
-		if(prevScope)
-	            symTable = prevScope->SymbolTable;
-            }
-	}
-        return std::nullopt;
-    }
+void STAB::Scope::createFnBlock() {
+  F = Builder->GetInsertBlock()->getParent();
 }
 
+llvm::Function *STAB::Scope::getFnBlock() { return F; }
+std::string STAB::Scope::getName() const { return name; }
+
+// install variable name to the idTable
+void STAB::Scope::installID(std::string key, valType value) {
+  SymbolTable.insert({key, value});
+}
+
+// check if a variable name already exists within the current scope
+std::optional<valType> STAB::Scope::getIdCurrentScope(std::string id) const {
+  auto found = SymbolTable.find(id);
+  if (found != SymbolTable.end()) {
+    return found->second;
+  }
+  return std::nullopt;
+}
+
+// check if a variable name already exists within the reachable scope
+std::optional<valType> STAB::Scope::getID(std::string id) const {
+  Scope *prevScope = prev;
+  auto found = SymbolTable.find(id);
+  if (found != SymbolTable.end()) {
+    return found->second;
+  }
+  if (prevScope) {
+    auto symTable = prevScope->SymbolTable;
+    while (prevScope) {
+      auto found = symTable.find(id);
+      if (found != symTable.end()) {
+        return found->second;
+      }
+      prevScope = prevScope->prev;
+      if (prevScope)
+        symTable = prevScope->SymbolTable;
+    }
+  }
+  return std::nullopt;
+}
+} // namespace STAB
--- compiler/src/includes/scope.hpp
+++ compiler/src/includes/scope.hpp
@@ -3,38 +3,39 @@
 #ifndef STAB_SCOPE_HPP
 #define STAB_SCOPE_HPP
 
-#include <llvm/IR/SymbolTableListTraits.h>
-#include <variant>
-#include <optional>
-#include <map>
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Instructions.h"
 #include <llvm/IR/Function.h>
+#include <llvm/IR/SymbolTableListTraits.h>
+#include <map>
+#include <optional>
+#include <variant>
 
-namespace STAB{ 
-      using valType = std::variant<llvm::AllocaInst*, llvm::GlobalVariable*>;
-    class Scope {
-       std::map<std::string, valType> SymbolTable;
-       std::string name;
-       llvm::Function* F;
-      public:
-       Scope* prev;
-       Scope(Scope* e = nullptr, std::string name = ""):name(name), prev(e){}
+namespace STAB {
+using valType = std::variant<llvm::AllocaInst *, llvm::GlobalVariable *>;
+class Scope {
+  std::map<std::string, valType> SymbolTable;
+  std::string name;
+  llvm::Function *F;
 
-       void setFnBlock(llvm::Function* Fn);
+public:
+  Scope *prev;
+  Scope(Scope *e = nullptr, std::string name = "") : name(name), prev(e) {}
 
-       void createFnBlock();
+  void setFnBlock(llvm::Function *Fn);
 
-       llvm::Function* getFnBlock();
-       std::string getName() const;
+  void createFnBlock();
 
-       // install variable name to the idTable
-       void installID(std::string key, valType value);
+  llvm::Function *getFnBlock();
+  std::string getName() const;
 
-       // check if a variable name already exists within the current scope
-       std::optional<valType> getID(std::string id) const;
+  // install variable name to the idTable
+  void installID(std::string key, valType value);
 
- };
+  // check if a variable name already exists within the reachable scope
+  std::optional<valType> getID(std::string id) const;
+  std::optional<valType> getIdCurrentScope(std::string id) const;
+};
 
-}
+} // namespace STAB
 #endif
